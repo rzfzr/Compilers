c85e486105fa8f06af2577cebb4d59fe6ca2426f
Rafael Figueira Goncalves <capitao.rafa@gmail.com>
2018-04-16 20:55:28 -0300

    Moving

--- lectures/first-translator/myTranslator.cpp
+++ lectures/first-translator/myTranslator.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <cctype>
 #include <cstdlib>
-#include "mini_lex.cpp"
+#include "../myLexical.cpp"
 
 using namespace std;
 
@@ -14,50 +14,71 @@ void print(char);
 
 token lookahead;
 
-void print_char(char c){
+void print_char(char c)
+{
     printf("%c ", c);
 }
 
-void print_int(int i){
+void print_int(int i)
+{
     printf("%d ", i);
 }
 
-void list(){
+void list()
+{
     term();
     x();
 }
 
-void x(){
-    if (lookahead.type == SIGN) {
+void x()
+{
+    if (lookahead.type == SIGN)
+    {
         int value = lookahead.value;
-        match(SIGN); term(); print_char((char)value); x();
-    } else if (lookahead.type == EOF){
+        match(SIGN);
+        term();
+        print_char((char)value);
+        x();
+    }
+    else if (lookahead.type == EOF)
+    {
         printf("\nSuccess.\n");
-    } else {
+    }
+    else
+    {
         printf("Syntax error on x\n");
         exit(1);
     }
 }
 
-void term(){
-    if (lookahead.type == NUM) {
+void term()
+{
+    if (lookahead.type == NUM)
+    {
         print_int(lookahead.value);
         match(NUM);
-    } else {
+    }
+    else
+    {
         printf("Syntax error on term\n");
         exit(1);
     }
 }
 
-void match(int type){
-    if (lookahead.type == type){
+void match(int type)
+{
+    if (lookahead.type == type)
+    {
         lookahead = next_token();
-    } else {
+    }
+    else
+    {
         printf("Match error");
     }
 }
 
-int main(){
+int main()
+{
     input = "123+321 -  111";
     lookahead = next_token();
     list();
